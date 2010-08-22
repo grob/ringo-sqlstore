cbd7ad02747711203e63c8bee21d97a64bd36a9d
Robert Gaggl <robert@nomatic.org>
2010-08-22 13:17:11 +0200

    added postgresql unit tests

--- test/all.js
+++ test/all.js
@@ -2,6 +2,7 @@ exports.testCache = require("./cache_test");
 exports.testH2 = require("./h2_test");
 exports.testMysql = require("./mysql_test");
 exports.testOracle = require("./oracle_test");
+exports.testOracle = require("./postgresql_test");
 
 //start the test runner if we're called directly from command line
 if (require.main == module.id) {
