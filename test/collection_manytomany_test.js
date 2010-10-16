c2d4f8889974c71dfccbc9a33d0e834fc4b0f501
Robert Gaggl <robert@nomatic.org>
2010-10-16 14:35:07 +0200

    Implemented many-to-many collections, query joins and filters

---
+++ test/collection_manytomany_test.js
@@ -0,0 +1,152 @@
+var assert = require("assert");
+
+var Store = require("ringo/storage/sql/store").Store;
+var sqlUtils = require("ringo/storage/sql/util");
+var store = null;
+var Author = null;
+var Book = null;
+var Relation = null;
+var dbProps = {
+    "url": "jdbc:h2:mem:test",
+    "driver": "org.h2.Driver"
+};
+
+const MAPPING_AUTHOR = {
+    "id": {
+        "column": "author_id"
+    },
+    "properties": {
+        "name": {
+            "type": "string",
+            "nullable": false
+         },
+         "books": {
+            "type": "collection",
+            "entity": "Book",
+            "through": "Relation",
+            "join": "Relation.book = Book.id",
+            "foreignProperty": "Relation.author"
+         }
+    }
+};
+
+const MAPPING_BOOK = {
+    "id": {
+        "column": "book_id"
+    },
+    "properties": {
+        "title": {
+            "type": "string",
+            "column": "book_title",
+            "length": 255,
+            "nullable": false,
+        },
+        "authors": {
+            "type": "collection",
+            "entity": "Author",
+            "through": "Relation",
+            "join": "Relation.author = Author.id",
+            "foreignProperty": "Relation.book"
+        }
+    }
+};
+
+const MAPPING_RELATION = {
+    "id": {
+        "column": "rel_id"
+    },
+    "properties": {
+        "author": {
+            "type": "object",
+            "entity": "Author",
+            "column": "rel_author",
+            "nullable": false
+        },
+        "book": {
+            "type": "object",
+            "entity": "Book",
+            "column": "rel_book",
+            "nullable": false
+        }
+    }
+};
+
+exports.setDbProps = function(props) {
+    dbProps = props;
+    return;
+};
+
+exports.setUp = function() {
+    store = new Store(dbProps);
+    Author = store.defineEntity("Author", MAPPING_AUTHOR);
+    Book = store.defineEntity("Book", MAPPING_BOOK);
+    Relation = store.defineEntity("Relation", MAPPING_RELATION);
+    return;
+};
+
+exports.tearDown = function() {
+    var conn = store.getConnection();
+    [Author, Book, Relation].forEach(function(ctor) {
+        var schemaName = ctor.mapping.schemaName || store.dialect.getDefaultSchema(conn);
+        if (sqlUtils.tableExists(conn, ctor.mapping.tableName, schemaName)) {
+            sqlUtils.dropTable(conn, store.dialect, ctor.mapping.tableName, schemaName);
+            if (ctor.mapping.id.hasSequence() && store.dialect.hasSequenceSupport()) {
+                sqlUtils.dropSequence(conn, store.dialect, ctor.mapping.id.sequence, schemaName);
+            }
+        }
+    });
+    store.connectionPool.stopScheduler();
+    store.connectionPool.closeConnections();
+    store = null;
+    Author = null;
+    Book = null;
+    Relation = null;
+    return;
+};
+
+exports.testSimpleManyToMany = function() {
+    var transaction = store.createTransaction();
+    var authors = [];
+    var books = [];
+    for (var i=1; i<3; i+=1) {
+        var author = new Author({
+            "name": "Author " + 1
+        });
+        author.save(transaction);
+        authors.push(author);
+        var book = new Book({
+            "title": "Book " + i
+        });
+        book.save(transaction);
+        books.push(book);
+    }
+    var relations = [];
+    books.forEach(function(book) {
+        authors.forEach(function(author, idx) {
+            var relation = new Relation({
+                "book": book,
+                "author": author
+            });
+            relation.save(transaction);
+            relations.push(relation);
+        });
+    })
+    transaction.commit();
+    var book = Book.get(1);
+    assert.isNotNull(book);
+    assert.strictEqual(book.authors.length, 2);
+    assert.strictEqual(book.authors.get(0)._id, authors[0]._id);
+    assert.strictEqual(book.authors.get(1)._id, authors[1]._id);
+    var author = Author.get(2);
+    assert.isNotNull(author);
+    assert.strictEqual(author.books.length, 2);
+    assert.strictEqual(author.books.get(0)._id, books[0]._id);
+    assert.strictEqual(author.books.get(1)._id, books[1]._id);
+    return;
+};
+
+
+//start the test runner if we're called directly from command line
+if (require.main == module.id) {
+    require("test").run(exports);
+}
--- test/suite.js
+++ test/suite.js
@@ -3,5 +3,7 @@
 exports = [
     "transaction_test",
     "store_test",
-    "collection_test"
+    "collection_test",
+    "collection_manytomany_test",
+    "query_join_test"
 ];
