a71a429563a82adec57769dcc8212dc653f54bb3
Robert Gaggl <robert@nomatic.org>
2011-10-07 12:10:08 +0200

    modified connection pool to properly remove closed connections

--- test/connectionpool_test.js
+++ test/connectionpool_test.js
@@ -25,7 +25,7 @@ exports.testGetConnection = function() {
     conn1.close();
     assert.isFalse(conn1.isInUse());
     assert.strictEqual(pool.size(), 1);
-    
+
     // retrieve connection again - must be above connection, since it has been closed
     var conn2 = pool.getConnection();
     assert.strictEqual(conn1, conn2);
@@ -46,6 +46,29 @@ exports.testIsStale = function() {
     assert.isTrue(conn.isStale(0));
     assert.isFalse(conn.isStale(1000));
     return;
+};
+
+exports.testRemoveDeadConnection = function() {
+    assert.strictEqual(pool.size(), 0);
+    var conn = pool.getConnection();
+    assert.strictEqual(pool.size(), 1);
+    assert.isTrue(conn.isValid());
+    // close underlying connection and return it to the pool - since the
+    // connection is dead now, it must be removed from the pool when
+    // calling conn.close()
+    conn.getConnection().close();
+    conn.close();
+    assert.strictEqual(pool.size(), 0);
+};
+
+exports.testConnectionIsValid = function() {
+    var conn = pool.getConnection();
+    assert.isTrue(conn.isValid());
+    assert.isTrue(conn.validate());
+    // close underlying connection
+    conn.getConnection().close();
+    assert.isFalse(conn.isValid());
+    assert.isFalse(conn.validate());
 };
 
 //start the test runner if we're called directly from command line
