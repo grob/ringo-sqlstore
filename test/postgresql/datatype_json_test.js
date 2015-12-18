c7b639fea83c64366fea1ccfcd4e5f36f1428e98
Philipp Naderer <philipp.naderer@gmail.com>
2015-12-18 16:51:53 +0100

    Adds a query test for JSON data

--- test/postgresql/datatype_json_test.js
+++ test/postgresql/datatype_json_test.js
@@ -7,12 +7,14 @@ var sqlUtils = require("../../lib/sqlstore/util");
 
 const MAPPING_EVENT_JSON = {
     "properties": {
+        "slug": "string",
         "data": "json"
     }
 };
 
 const MAPPING_EVENT_JSONB = {
     "properties": {
+        "slug": "string",
         "data": "jsonb"
     }
 };
@@ -40,9 +42,11 @@ exports.tearDown = function() {
 
 exports.testSaveObject = function() {
     var event = new Event({
+        "slug": "some event",
         "data": { "propertyString": "event1", "propertyNumber": 12345 }
     });
     var eventb = new EventB({
+        "slug": "some event",
         "data": { "propertyString": "event2", "propertyNumber": 67890 }
     });
     event.save();
@@ -57,6 +61,72 @@ exports.testSaveObject = function() {
     assert.strictEqual(EventB.all().length, 1);
 
     return;
+};
+
+exports.testQueryObjects = function() {
+    // populate the tables
+    store.beginTransaction();
+    for (var i = 0; i < 10; i++) {
+        let obj = { "title": "event" + i, "num": Math.random() };
+        if (i % 2) {
+            obj.rating = i;
+        }
+        let event = new Event({
+            "slug": "event" + i,
+            "data": obj
+        });
+        let eventb = new EventB({
+            "slug": "event" + i,
+            "data": obj
+        });
+        event.save();
+        eventb.save();
+    }
+    store.commitTransaction();
+
+    let query = "select Event.slug as slug, Event.data as data from Event order by Event.id";
+    let result = store.query(query);
+    assert.strictEqual(result.length, 10, query);
+    assert.strictEqual(result[0].slug, "event0", query);
+    assert.strictEqual(result[0].data.title, "event0", query);
+
+    query = "select Event.slug from Event where Event.slug = 'event1'";
+    result = store.query(query);
+    assert.strictEqual(result.length, 1, query);
+
+    query = "select EventB.slug as slug, EventB.data as data from EventB order by EventB.id";
+    result = store.query(query);
+    assert.strictEqual(result.length, 10, query);
+    assert.strictEqual(result[0].slug, "event0", query);
+    assert.strictEqual(result[0].data.title, "event0", query);
+
+    query = "select EventB.slug from EventB where EventB.slug = 'event1'";
+    result = store.query(query);
+    assert.strictEqual(result.length, 1, query);
+
+    let nativeQuery = "SELECT * FROM \"Event\" WHERE (data->'rating')IS NOT NULL";
+    result = store.sqlQuery(nativeQuery);
+
+    assert.strictEqual(result.length, 5, nativeQuery);
+    assert.strictEqual(result[0].id, 2, "wrong id");
+    assert.strictEqual(result[0].slug, "event1", "wrong slug");
+    assert.strictEqual(result[0].data.rating, 1, "wrong rating");
+
+    assert.strictEqual(result[1].id, 4, "wrong id");
+    assert.strictEqual(result[1].slug, "event3", "wrong slug");
+    assert.strictEqual(result[1].data.rating, 3, "wrong rating");
+
+    nativeQuery = "SELECT * FROM \"EventB\" WHERE (data->'rating')IS NOT NULL";
+    result = store.sqlQuery(nativeQuery);
+
+    assert.strictEqual(result.length, 5, nativeQuery);
+    assert.strictEqual(result[0].id, 2, "wrong id");
+    assert.strictEqual(result[0].slug, "event1", "wrong slug");
+    assert.strictEqual(result[0].data.rating, 1, "wrong rating");
+
+    assert.strictEqual(result[1].id, 4, "wrong id");
+    assert.strictEqual(result[1].slug, "event3", "wrong slug");
+    assert.strictEqual(result[1].data.rating, 3, "wrong rating");
 };
 
 if (require.main === module) {
