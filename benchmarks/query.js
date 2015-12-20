e332db042bba745a7baf2e4dd230b0e34bca0a6d
Robert Gaggl <robert@nomatic.org>
2015-12-20 12:22:15 +0100

    adapted to current version, insert just 1000 storables

--- benchmarks/query.js
+++ benchmarks/query.js
@@ -1,12 +1,12 @@
 var term = require("ringo/term");
 var assert = require("assert");
 
-var {Store, ConnectionPool, Cache} = require("../lib/main");
+var {Store, Cache} = require("../lib/main");
 var utils = require("../test/utils");
 
 var store = null;
 var Author = null;
-var maxAuthors = 10000;
+var maxAuthors = 1000;
 
 var MAPPING_AUTHOR = {
     // "schema": "TEST",
@@ -25,11 +25,11 @@ var MAPPING_AUTHOR = {
 };
 
 exports.setUp = function(dbProps) {
-    store = new Store(new ConnectionPool(dbProps));
+    store = new Store(Store.initConnectionPool(dbProps));
     term.writeln("------------------------------");
-    term.writeln("Using", store.connectionPool.getDriverClass());
+    term.writeln("Using", store.connectionPool.getDriverClassName());
     store.setQueryCache(new Cache(10000));
-    store.setEntityCache(new Cache(10000));
+    //store.setEntityCache(new Cache(10000));
     Author = store.defineEntity("Author", MAPPING_AUTHOR);
     store.syncTables();
     store.beginTransaction();
@@ -51,11 +51,11 @@ exports.start = function(cnt) {
     var start = Date.now();
     for (let i=0; i<cnt; i+=1) {
         let id = ((Math.random() * maxAuthors) | 0) + 1;
-        let author = store.query("select Author.* from Author where Author.id = :id", {
+        let author = store.query("select * from Author where id = :id", {
             "id": id
         })[0];
         assert.strictEqual(author.id, id);
     }
     var millis = Date.now() - start;
-    term.writeln(term.GREEN, cnt, "queries,", millis / cnt + "ms/query", term.RESET);
+    term.writeln(term.GREEN, millis, "ms for", cnt, "queries,", millis / cnt + "ms/query", term.RESET);
 };
