d9b082f07c575272057b7de85c9cd684e0234cb5
Robert Gaggl <robert@nomatic.org>
2015-12-20 09:31:37 +0100

    cleanup database config

--- test/config.js
+++ test/config.js
@@ -1,6 +1,8 @@
 exports.h2 = {
     "url": "jdbc:h2:mem:test;MVCC=TRUE",
-    "driver": "org.h2.Driver"
+    "driver": "org.h2.Driver",
+    "minimumIdle": 10,
+    "maximumPoolSize": 30
 };
 
 exports.mysql = {
@@ -8,22 +10,6 @@ exports.mysql = {
 //    "url": "jdbc:mysql://localhost/test?cachePrepStmts=true&prepStmtCacheSize=50&cacheResultSetMetadata=true&cacheServerConfiguration=true&dontTrackOpenResources=true&elideSetAutoCommits=true",
     "url": "jdbc:mysql://localhost/test",
     "driver": "com.mysql.jdbc.Driver",
-/*
-    "pool": "c3p0",
-    "minPoolSize": 20,
-    "initialPoolSize": 20,
-    "acquireIncrement": 5,
-    "numHelperThreads": 4,
-    "maxPoolSize": 100,
-    "maxStatementsPerConnection": 10,
-*/
-/*
-    "pool": "boneCP",
-    "minConnectionsPerPartition": 5,
-    "acquireIncrement": 5,
-    "maxConnectionsPerPartition": 20,
-    "partitionCount": 1,
-*/
     "user": "test",
     "password": "test"
 };
@@ -36,7 +22,6 @@ exports.oracle = {
 };
 
 exports.postgresql = {
-    "pool": "hikari",
     "url": "jdbc:postgresql://localhost/test",
     "driver": "org.postgresql.Driver",
     "user": "test",
